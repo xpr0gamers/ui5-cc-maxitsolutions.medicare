b086b9cb7aa1dd91e5eec8e69b71492f9f060e41
Maximilian Dietz <maxidietz96@gmail.com>
2022-09-14 16:13:51 +0200

    initial

--- Datepicker.js
+++ Datepicker.js
@@ -5,21 +5,8 @@ sap.ui.define(
         "sap/m/Link",
         "sap/m/Button",
         "sap/base/util/deepEqual"
-    ], (Control) => {
+    ], (Control, DatePicker, Link, Button, deepEqual) => {
         return Control.extend("cc.maxitsolutions.medicare.Datepicker", {
-            renderer: {
-                apiVersion: 2,
-                /**
-                * @param {sap.ui.core.RenderManager} oRM
-                * @param {sap.ui.core.Control} oControl
-                */
-                render(oRM, oControl) {
-                    oRM.openStart("p", oControl)
-                    oRM.openEnd()
-                    oRM.text("UI5 custom control: Hello World!")
-                    oRM.close("p")
-                }
-            },
             metadata: {
                 events: {
                     change: {
@@ -29,7 +16,7 @@ sap.ui.define(
                     }
                 },
                 properties: {
-                    sType: { type: "string" }, //days, weeks, mooths
+                    sType: { type: "string", defaultValue: "days" }, //days, weeks, months
                     oDate: { type: "object" },
                 },
                 aggregations: {
@@ -39,39 +26,23 @@ sap.ui.define(
                     _buttonRight: { type: "sap.m.Button", multiple: false, visibility: "hidden" },
                 }
             },
-            setSType: function (sType) {
-                this.setProperty("sType", sType, true);
-                switch (this.getProperty("sType")) {
-                    case 'days':
-                        //todo
-                        break;
-                    case 'weeks':
-                        //todo
-                        break;
-                    case 'months':
-                        this.getAggregation("_datePicker").setDisplayFormat("MM-y");
-                        break;
-                }
-            },
-            setODate: function (oDate) {
-                if (oDate == null) {
-                    throw new Error("Unexpected value detected!");
-                }
-                this.setProperty("oDate", oDate, true);
-                switch (this.getProperty("sType")) {
-                    case 'days':
-                        var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "EEEE dd.MM.yyyy" });
-                        this.getAggregation("_text").setText(oDateFormat.format(oDate.dDateVon));
-                        break;
-                    case 'weeks':
-                        var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "dd.MM.yyyy" });
-                        this.getAggregation("_text").setText(oDateFormat.format(oDate.dDateVon) + " - " + oDateFormat.format(oDate.dDateEnde));
-                        break;
-                    case 'months':
-                        var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "MM.yyyy" });
-                        this.getAggregation("_text").setText(oDateFormat.format(oDate.dDateVon));
-                        this.getAggregation("_datePicker").setDateValue(oDate.dDateVon);
-                        break;
+            renderer: {
+                apiVersion: 2,
+                /**
+                * @param {sap.ui.core.RenderManager} oRm
+                * @param {sap.ui.core.Control} oControl
+                */
+                render(oRm, oControl) {
+                    oRm.openStart("div", oControl);
+                    oRm.style("display", "flex");
+                    oRm.style("flex-direction", "row");
+                    oRm.style("align-items", "center");
+                    oRm.style("justify-content", "center");
+                    oRm.openEnd();
+                    oRm.renderControl(oControl.getAggregation("_buttonLeft"))
+                    oRm.renderControl(oControl.getAggregation("_text"));
+                    oRm.renderControl(oControl.getAggregation("_buttonRight"));
+                    oRm.close("div")
                 }
             },
             init: function () {
@@ -87,6 +58,9 @@ sap.ui.define(
                 this.setAggregation("_text", new Link({
                     subtle: true,
                     emphasized: true,
+                    width: "80px",
+                    textAlign: "Center",
+                    wrapping: true,
                     press: (oEvent) => {
                         that.getAggregation("_datePicker").openBy(oEvent.getSource().getDomRef());
                     }
@@ -106,27 +80,75 @@ sap.ui.define(
                         that.onDateChange(1, oDateCurrent);
                     }
                 }).addStyleClass("sapUiTinyMargin"));
+
+                //set default value for date
+                if (this.getODate() == null) {
+                    const oDefaultDateValue = this.getDateForType(this.getSType(), { dDateVon: new Date() }, 0);
+                    this.setODate(oDefaultDateValue);
+                }
+            },
+            setSType: function (sType) {
+                //validation
+                if (["days", "weeks", "months"].includes(sType) == false) {
+                    throw new Error("Unexpected value detected for sType!");
+                }
+                this.setProperty("sType", sType, true);
+                this.syncAggregations();
+            },
+            setODate: function (oDate) {
+                //validation
+                if (oDate == null) {
+                    throw new Error("Unexpected value detected for oData!");
+                }
+                this.setProperty("oDate", oDate, true);
+                this.syncAggregations();
+            },
+            syncAggregations: function () {
+                const sType = this.getProperty("sType");
+                const oDate = this.getProperty("oDate");
+                switch (sType) {
+                    case 'days':
+                        var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "EEEE dd.MM.yyyy" });
+                        this.getAggregation("_text").setText(oDateFormat.format(oDate.dDateVon));
+                        this.getAggregation("_datePicker").setDisplayFormat("dd.MM.yyyy");
+                        this.getAggregation("_datePicker").setDateValue(oDate.dDateVon);
+                        break;
+                    case 'weeks':
+                        var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "dd.MM.yyyy" });
+                        this.getAggregation("_text").setText(oDateFormat.format(oDate.dDateVon) + " - " + oDateFormat.format(oDate.dDateEnde));
+                        this.getAggregation("_datePicker").setDisplayFormat("dd.MM.yyyy");
+                        this.getAggregation("_datePicker").setDateValue(oDate.dDateVon);
+                        break;
+                    case 'months':
+                        var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "MMMM MM.yyyy" });
+                        this.getAggregation("_text").setText(oDateFormat.format(oDate.dDateVon));
+                        this.getAggregation("_datePicker").setDisplayFormat("MM-y");
+                        this.getAggregation("_datePicker").setDateValue(oDate.dDateVon);
+                        break;
+                }
+            },
+            getDateForType: function (sType, oDate, nChange) {
+                switch (sType) {
+                    case 'days':
+                        var dDate = new Date(oDate.dDateVon);
+                        dDate.setDate(dDate.getDate() + nChange);
+                        return this.getDayBordersOfDate(dDate)
+                    case 'weeks':
+                        var dDate = new Date(oDate.dDateVon);
+                        dDate.setDate(dDate.getDate() + (7 * nChange));
+                        return this.getWeekBordersOfDate(dDate)
+                    case 'months':
+                        const oOtherMonth = this.getPrevOrNextMonth(oDate.dDateVon, nChange);
+                        return this.getMonthBordersOfDate(oOtherMonth);
+                    default:
+                        throw new Error(sType + " is not a valid type for this Datepicker");
+                }
             },
             onDateChange: function (nChange, oDateNew) {
                 //calulate new result date
-                var oResultDate;
-                switch (this.getProperty("sType")) {
-                    case 'days':
-                        var dDate = new Date(oDateNew.dDateVon);
-                        oDate.setDate(dDate.getDate() + nChange);
-                        oResultDate = this.getDayBordersOfDate(dDate)
-                        break;
-                    case 'weeks':
-                        var dDate = new Date(oDateNew.dDateVon);
-                        oDate.setDate(dDate.getDate() + 7 * nChange);
-                        oResultDate = this.getWeekBordersOfDate(dDate)
-                        break;
-                    case 'months':
-                        const oOtherMonth = this.getPrevOrNextMonth(oDateNew.dDateVon, nChange);
-                        oResultDate = this.getMonthBordersOfDate(oOtherMonth);
-                        break;
-                }
+                var oResultDate = this.getDateForType(this.getProperty("sType"), oDateNew, nChange);
                 //check if date is changed and only set and fire event when changed
+                //This happens, when original datepicker is opened
                 var oDateCurrent = this.getProperty("oDate");
                 if (deepEqual(oDateCurrent, oResultDate)) {
                     return;
